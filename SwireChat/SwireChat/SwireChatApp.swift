b506af2b5a350ddd6217f566fc0e4083cdb9a880
dennymathewfp <denny.mathew@fairprice.com.sg>
2022-01-31 17:46:32 +0800

    [SWC-01] Setting up

---
+++ SwireChat/SwireChat/SwireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwireChatApp.swift
+//  SwireChat
+//
+//  Created by MATHEW DENNY on 31/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SwireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
